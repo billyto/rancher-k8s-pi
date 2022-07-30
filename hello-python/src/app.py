9db29b0447f1326e714e52b4659f0cda1abe684e
Billy Tobon <billy.tobon@gmail.com>
2022-07-30 12:49:34 -0400

    cluster working

--- hello-python/src/app.py
+++ hello-python/src/app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_afrika():
+    return 'Hello Afrika - Tell me how you\'re doin\'!'
